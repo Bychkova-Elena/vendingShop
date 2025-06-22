cc3dd70618721749e370fc870a73c7c722cde1d6
Елена Бычкова <e.bychkova@vk.team>
2025-06-22 19:43:10 +0300

    добавила мапинг для ячеек

--- src/main/java/com/bychkova/elena/Vending/controller/CellController.java
+++ src/main/java/com/bychkova/elena/Vending/controller/CellController.java
@@ -2,6 +2,7 @@ package com.bychkova.elena.Vending.controller;
 import com.bychkova.elena.Vending.dto.CellResponse;
 import com.bychkova.elena.Vending.dto.addProductInCellRq;
 import com.bychkova.elena.Vending.entity.Cell;
+import com.bychkova.elena.Vending.mapper.CellMapper;
 import com.bychkova.elena.Vending.service.CellService;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
@@ -16,46 +17,26 @@ public class CellController {
     @Autowired
     private CellService cellService;
 
+    @Autowired
+    private CellMapper mapper;
+
     @GetMapping
     public Iterable<CellResponse> getAllCells() {
 
-        return convertListToResponse(cellService.getAllCells());
+        return mapper.convertListToResponse(cellService.getAllCells());
     }
 
     @PutMapping("/{id}/add/product")
     public CellResponse putProductInCell(@PathVariable Long id,
                                          @Valid @RequestBody addProductInCellRq productInCellRq) {
         Cell cell = cellService.putProductInCell(id, productInCellRq.getProductId(), productInCellRq.getQuantity());
-        return convertToResponse(cell);
+        return mapper.convertToResponse(cell);
     }
 
     @PutMapping("{id}/decrease/product")
     public CellResponse decreaseProductInCell(@PathVariable Long id) {
         Cell cell = cellService.decreaseProductInCell(id);
 
-        return convertToResponse(cell);
-    }
-
-    private CellResponse convertToResponse(Cell c) {
-        CellResponse cellResponse = new CellResponse();
-        cellResponse.setId(c.getId());
-        cellResponse.setVendingId(c.getVending().getId());
-        if(c.getProduct() != null) {
-            cellResponse.setProductId(c.getProduct().getId());
-        }
-        cellResponse.setFreePlacesCount(c.getFreePlacesCount());
-
-        return cellResponse;
-    }
-
-    private Iterable<CellResponse> convertListToResponse(Iterable<Cell> cells) {
-        ArrayList<CellResponse> cellList = new ArrayList<>();
-
-        for (Cell c : cells) {
-            CellResponse cellResponse = convertToResponse(c);
-            cellList.add(cellResponse);
-        }
-
-        return cellList;
+        return mapper.convertToResponse(cell);
     }
 }
---
+++ src/main/java/com/bychkova/elena/Vending/mapper/CellMapper.java
@@ -0,0 +1,33 @@
+package com.bychkova.elena.Vending.mapper;
+
+import com.bychkova.elena.Vending.dto.CellResponse;
+import com.bychkova.elena.Vending.entity.Cell;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class CellMapper {
+    public CellResponse convertToResponse(Cell c) {
+        CellResponse cellResponse = new CellResponse();
+        cellResponse.setId(c.getId());
+        cellResponse.setVendingId(c.getVending().getId());
+        if(c.getProduct() != null) {
+            cellResponse.setProductId(c.getProduct().getId());
+        }
+        cellResponse.setFreePlacesCount(c.getFreePlacesCount());
+
+        return cellResponse;
+    }
+
+    public Iterable<CellResponse> convertListToResponse(Iterable<Cell> cells) {
+        ArrayList<CellResponse> cellList = new ArrayList<>();
+
+        for (Cell c : cells) {
+            CellResponse cellResponse = convertToResponse(c);
+            cellList.add(cellResponse);
+        }
+
+        return cellList;
+    }
+}
